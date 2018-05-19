37a8a20172311b7ebe1ddebf4c93a4cb05cdbf3a
mirka <mirkarimov@gmail.com>
2018-05-18 20:22:36 -0700

    modified comments

--- staticsite/staticsite.go
+++ staticsite/staticsite.go
@@ -1,3 +1,17 @@
+// Command staticsite is a trivial web server that serves static content
+// placed under directory with name static. 
+// 
+// It should be executed from the directory in which the source resides,
+// as it will look for "static" directory  and its files in the current directory.
+//
+// Content access
+//
+// For testing purposes an access to the static content is possible in two different ways:
+// simply from the root / and as /app/.
+//     apphandler := http.FileServer(http.Dir("./static"))
+//     http.Handle("/app/", http.StripPrefix("/app/", apphandler))
+//
+//https://stackoverflow.com/questions/27945310/why-do-i-need-to-use-http-stripprefix-to-access-my-static-files
 package main
 
 import (
